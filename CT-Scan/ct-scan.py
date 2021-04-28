a4c119441c34015a081490d36547e9e9ded6f81d
micu <andrei.micuda@gmail.com>
2021-04-28 23:25:09 +0300

    Various cnn architectures

--- CT-Scan/ct-scan.py
+++ CT-Scan/ct-scan.py
@@ -4,7 +4,7 @@ import PIL
 import PIL.Image
 import tensorflow as tf
 import pathlib
-from tensorflow.keras import layers, models, preprocessing, callbacks, regularizers
+from tensorflow.keras import layers, models, preprocessing, callbacks, regularizers, constraints
 import matplotlib.pyplot as plt
 
 
@@ -36,18 +36,25 @@ def load_data(dir_name, csv_file):
 def preprocess_images(imgs):
     processed_images = np.copy(imgs)
     # processed_images = processed_images / 255.0
+    print(processed_images.shape)
+    # processed_images = 255 - processed_images
     mean = np.mean(processed_images, axis=(1, 2), keepdims=True)
     std = np.std(processed_images, axis=(1, 2), keepdims=True)
     processed_images = (processed_images - mean) / std
     return processed_images
+    # ones = np.ones(2500).reshape((50, 50))
+    # return np.subtract(ones, processed_images)
 
 
 def show_data_sample(imgs, labels):
     plt.figure(figsize=(10, 10))
     for i in range(9):
         ax = plt.subplot(3, 3, i + 1)
+        # ones = np.ones(2500).reshape((50, 50))
+        # print()
         plt.imshow(imgs[i])
-        plt.title(class_names[labels[i]])
+        # plt.imshow(np.subtract(ones, imgs[i]))
+        # plt.title(class_names[labels[i]])
         plt.axis("off")
     plt.show()
 
@@ -120,9 +127,10 @@ validation_labels = tf.keras.utils.to_categorical(validation_labels)
 #     plt.colorbar()
 # plt.show()
 
-callback = callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+callback = callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 
 def add_layer(model, layer, is_input_layer=False, is_output_layer=False):
+    weight_decay = 1e-4
     parts = [part.strip() for part in layer.split("~")]
     for part in parts:
         if part.startswith("CL"):
@@ -138,7 +146,11 @@ def add_layer(model, layer, is_input_layer=False, is_output_layer=False):
                     input_shape=(img_height, img_width, img_channels),
                     kernel_regularizer=regularizers.l2()))
             else:
-                model.add(layers.Conv2D(filters, (kernel_size, kernel_size), activation="relu", kernel_regularizer=regularizers.l2()))
+                model.add(layers.Conv2D(
+                    filters,
+                    (kernel_size, kernel_size),
+                    activation="relu",
+                    kernel_regularizer=regularizers.l2(weight_decay)))
         elif part.startswith("P"):
             pool_size = int(part[1:])
             # model.add(layers.AvgPool2D(pool_size=(pool_size, pool_size)))
@@ -152,7 +164,7 @@ def add_layer(model, layer, is_input_layer=False, is_output_layer=False):
             model.add(layers.Flatten())
         elif part.startswith("D"):
             units = int(part[1:])
-            model.add(layers.Dense(units, activation="softmax" if is_output_layer else "relu"))
+            model.add(layers.Dense(units, activation="softmax" if is_output_layer else "relu", kernel_constraint=constraints.MaxNorm(3)))
 
 
 
@@ -161,17 +173,67 @@ def create_model(pattern):
     layers = [layer.strip() for layer in pattern.split("->")]
     num_layers = len(layers)
     model.add(tf.keras.layers.experimental.preprocessing.Normalization(input_shape=(50, 50, 1)))
-    model.add(tf.keras.layers.experimental.preprocessing.RandomContrast(factor=0.1))
+    # model.add(tf.keras.layers.experimental.preprocessing.RandomContrast(factor=0.1))
     for i, layer in enumerate(layers):
         add_layer(model, layer, is_output_layer=(i == num_layers - 1))
     model.compile(
-        optimizer="adam",
-        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        optimizer=tf.keras.optimizers.Adam(),
+        loss=tf.keras.losses.CategoricalCrossentropy(),
         metrics=["accuracy"],
     )
     return model
 
-def model_testing(epochs):
+
+def model_testing(model_pattern, epochs):
+    # model_patterns = ["CL48C5~P2~BN~DO1 -> CL64C5~P2~BN~DO1 -> CL128C5~BN~DO1 -> F -> D512 -> D512 -> D3"]
+    model = create_model(model_pattern)
+
+    model.summary()
+
+    train_images = tf.expand_dims(preprocess_images(train_ds), axis=-1)
+    datagen = preprocessing.image.ImageDataGenerator(
+        # height_shift_range=[-5, 5],
+        horizontal_flip=True,
+        rotation_range=15,
+        # zoom_range=[0.7, 1.0]
+    )
+    train_images_it = datagen.flow(tf.expand_dims(train_ds, axis=-1), train_labels)
+    validation_images = tf.expand_dims(preprocess_images(validation_ds), axis=-1)
+
+    history = model.fit(
+        train_images_it,
+        epochs=epochs,
+        validation_data=(validation_images, validation_labels),
+        # callbacks=[callback],
+        # steps_per_epoch=468,
+        shuffle=True
+    )
+
+    # PLOT ACCURACIES
+    fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 12))
+    fig.suptitle(model_pattern)
+    ax1.plot(history.history["val_accuracy"], linestyle="solid", color="orange")
+    ax1.plot(history.history["accuracy"], linestyle="solid", color="blue")
+    ax1.set_xticks(range((epochs)))
+    ax1.set_title("Accuracies")
+    ax1.set_ylabel("accuracy")
+    ax1.set_xlabel("epoch")
+    ax1.legend(["val_accuracy", "accuracy"], loc="upper left")
+
+    ax2.plot(history.history["val_loss"], linestyle="solid", color="orange")
+    ax2.plot(history.history["loss"], linestyle="solid", color="blue")
+    ax2.set_xticks(range((epochs)))
+    ax2.set_title("Loss")
+    ax2.set_ylabel("loss")
+    ax2.set_xlabel("epoch")
+    ax2.legend(["val_loss", "loss"], loc="upper left")
+    # axes = plt.gca()
+    # axes.set_ylim([0.98, 1])
+    plt.show()
+
+
+
+def model_comparison(epochs):
     # model_patterns = ["CL48C5~P2~BN~DO1 -> CL64C5~P2~BN~DO1 -> CL128C5~BN~DO1 -> F -> D512 -> D512 -> D3"]
     model_patterns = [
         # "CL12C3~P2~BN~D40 -> CL24C3~P2~BN~D20 -> F -> D32 -> D16 -> D3",
@@ -180,10 +242,10 @@ def model_testing(epochs):
         # "CL10C5~P2~BN~D20 -> CL10C5~P2~BN~D20 -> F -> D16 -> D3",
         # "CL8C5~P2~BN~D20 -> CL10C5~P2~BN~D20 -> F -> D16 -> D3",
         # "CL8C5~P2~BN~D20 -> CL8C5~P2~BN~D20 -> F -> D16 -> D3",
-        "CL6C3~P2~BN~D40 -> CL6C3~P2~BN~D40 -> CL6C3~P2~BN~D20 -> F -> D24 -> D3",
+        #"CL6C3~P2~BN~D40 -> CL6C3~P2~BN~D40 -> CL6C3~P2~BN~D20 -> F -> D24 -> D3",
         # "CL6C5~P2~BN~D20 -> CL6C5~P2~BN~D20 -> F -> D16 -> D3",
-        # "CL12C3~P2~BN~D40 -> CL24C3~P2~BN~D20 -> F -> D64 -> D32 -> D3",
-        # "CL12C3~P2~BN~D40 -> CL24C3~P2~BN~D20 -> F -> D64 -> D64 -> D3",
+        # "CL12C3~BN~P2~DO40 -> CL24C3~BN~P2~DO20 -> F -> D64 -> D32 -> D3",
+        # "CL8C5~BN~P2~DO50 -> CL8C5~BN~P2~DO50 -> CL16C3~BN~P2~DO50 -> CL16C3~BN~P2~DO30 -> F -> D64 -> D64 -> D3",
     ]
     model_num = len(model_patterns)
     history = [0] * model_num
@@ -317,7 +379,7 @@ def data_augmentation_testing(epochs):
     plt.show()
 
 # data_augmentation_testing(20)
-model_testing(70)
+model_testing("CL12C7~CL12C7~BN~P2~DO50 -> CL24C5~CL24C5~BN~P2~DO50 -> F -> D120~DO50 -> D64~DO50 -> D3", 50)
 
 
 # history = [
